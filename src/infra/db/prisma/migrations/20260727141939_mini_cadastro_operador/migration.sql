2d8dcfa02fdd7276e8cfe6b17abf111aee873662
Henrique Campos <110436896+henriquedev14@users.noreply.github.com>
2026-07-27 11:21:13 -0300

    Create migration.sql

---
+++ src/infra/db/prisma/migrations/20260727141939_mini_cadastro_operador/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "operadores_producao" ADD COLUMN     "funcao" TEXT,
+ADD COLUMN     "observacoes" TEXT,
+ADD COLUMN     "telefone" TEXT,
+ADD COLUMN     "turno" "TurnoProducao";
